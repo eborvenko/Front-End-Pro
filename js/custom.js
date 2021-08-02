d4eb9ae38592a41d455d83c2a5805ed7b2de3b14
Your Name <eborvenko@gmial.com>
2021-08-02 18:56:36 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+var height = 40;
+var radius = 10;
+
+var volume = radius * radius * height * Math.PI;
+
+console.log ('Cylinder height --->' , height);
+console.log ('Cylinder radius --->' , radius);
+console.log ('Cylinder volume --->' , volume);
