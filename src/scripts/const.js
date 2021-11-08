30ad356f04dbc7f4435f88ae90e1a4c7f796bd4f
Ekaterina <37264440+eborvenko@users.noreply.github.com>
2021-11-08 19:29:26 +0200

    Delete src directory

--- src/scripts/const.js
+++
@@ -1,2 +0,0 @@
-export const STUDENT_URL =
-    'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/students';
--- src/scripts/index.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery';
-
-import Controller from './Controller';
-
-import '../styles/style.scss';
-
-new Controller($('#app'));
