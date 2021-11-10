dcf60fa023b01c607a994072777f86716979137c
Your Name <eborvenko@gmial.com>
2021-11-10 17:18:33 +0200

    HW35 Chat WebPack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry:'./src/index.js',
+    mode: 'development',
+    output:{
+        filename:'all.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist'),
+    },
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader', 'css-loader']
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template:'./src/index.html'
+    })],
+}
